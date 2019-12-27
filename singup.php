4dc62e10dfa4e962a1049eaeb19a3514c4d1a2aa
Vishwa Mahadevan <vishwamagadavan@gmail.com>
2019-12-27 16:17:29 +0530

    Update 26

---
+++ singup.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ICRTBI-20</title>
+</head>
+<body>
+    <center>Registration will be opened from 20<sup>th</sup> January 2020 Onwards</center>
+</body>
+</html>
+<?php 
+
+
+?>
