361ec78f92e9581e801a426d4b1877cc0df3c1f5
Vishwamagdavan <vishwamagadavan@gmail.com>
2020-02-12 11:26:13 +0530

    Register page Working

--- register.php
+++ register.php
@@ -1,106 +1,222 @@
-<link href="payment.css" rel="stylesheet" type="text/css"></link>
 <?php
 session_start();
-$con=mysqli_connect('localhost','root','','payment_db');
+$con = mysqli_connect("localhost", "u987684220_fdp", "admin@stjose", "u987684220_fdp");
 //require('config.php');
 // include('./httpful.phar');
 
 
 
-if(isset($_POST['submit'])){
-	$mtxnid = "STJOSEFDP".mt_rand();
-	$name=$_POST['element_1'];
-	$designation=$_POST['element_2'];
-	$email=$_POST['element_3'];
-	$institute=$_POST['element_4'];
-	$mobile=$_POST['element_5'];
-	$amount=$_POST['element_8'];
-	
-    	$query="INSERT into register set name='$name',designation='$designation',email='$email',institute='$institute',mobile='$mobile',amount='$amount',payment_id='$mtxnid'";
-    	$query_run=mysqli_query($con,$query);
-    	$_SESSION['mtxnid']=$mtxnid;
-        
-						
-		 header("Location: https://portal.stjosephstechnology.ac.in/sendPost.jsp?RUrl=https://portal.stjosephstechnology.ac.in/TechProcess?auth=fluffy%26amount=$amount%26user=ICRTET%26custid=$name%26refno=$mtxnid%26returnURL=http://www.stjosephstechnology.ac.in/icrtet2019.php", TRUE, 307);
-		
+if (isset($_POST['submit'])) {
+	$mtxnid = "STJOSEFDP" . mt_rand();
+	$name = $_POST['element_1'];
+	$designation = $_POST['element_2'];
+	$email = $_POST['element_3'];
+	$institute = $_POST['element_4'];
+	$mobile = $_POST['element_5'];
+	$amount = $_POST['element_8'];
+
+	$query = "INSERT into register set name='$name',designation='$designation',email='$email',institute='$institute',mobile='$mobile',amount='$amount',payment_id='$mtxnid'";
+	$query_run = mysqli_query($con, $query);
+	$_SESSION['mtxnid'] = $mtxnid;
+
+
+	header("Location: https://portal.stjosephstechnology.ac.in/sendPost.jsp?RUrl=https://portal.stjosephstechnology.ac.in/TechProcess?auth=fluffy%26amount=$amount%26user=ICRTET%26custid=$name%26refno=$mtxnid%26returnURL=http://icrtbi2020.stjosephstechnology.ac.in/register.php", TRUE, 307);
 }
 
 ?>
 
-
-
-
-
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>ICRTBI2020 Registration</title>
-<link rel="stylesheet" type="text/css" href="view.css" media="all">
-<script type="text/javascript" src="view.js"></script>
-<style type="text/css">
+<!DOCTYPE html>
+<html lang="en">
 
-</style>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ICRTBI2020 Registration</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="view.js"></script>
 </head>
-<body id="main_body" >
+
+<body>
+	<div class="container-fluid">
+		<center>
+			<h1><a>5th International Conference on Recent Trends in Engineering and Technology</a></h1>
+		</center>
+		<center>
+			<h2>St. Joseph's Insitute of Technology, Chennai,</h2>
+		</center>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div style="background-color: #e6e0e0" class="col-md-5">
+			<br>
+			<h4>Details</h4>
+			<form id="form_38599" class="appnitro" enctype="multipart/form-data" method="post" action="">
+					<div class="form-row">
+						<div class="form-group col-md-10">
+							<label for="inputEmail4">Paper ID</label>
+							<input type="text" class="form-control" id="element_0" placeholder="Paper" maxlength="255" value="">
+							<br>
+							<label for="inputPassword4">Paper title</label>
+							<input type="text" class="form-control" id="element_1" placeholder="Paper Title" maxlength="255" value="">
+							<br>
+							<label for="inputPassword4">Corresponding Author Name</label>
+							<input type="text" class="form-control" id="element_2" placeholder="Ex: John" maxlength="255" value="">
+							<br>
+							<label for="inputPassword4">Author Email</label>
+							<input type="email" class="form-control" id="element_3" placeholder="Ex: example@gmail.com" maxlength="255" value="">
+							<br>
+							<label for="inputPassword4">Name of Institution</label>
+							<input type="text" class="form-control" id="element_4" placeholder="St. Joseph's Institute of Technology" maxlength="255" value="">
+							<br>
+							<label for="inputPassword4">Author Mobile</label>
+							<input type="text" class="form-control" id="element_5" placeholder="Ex: +919898989898" maxlength="255" value="">
+						</div>
+						<div class="form-group col-md-6">
+							
+						</div>
+						<div class="form-group col-md-6">
+							<label for="inputPassword4">Password</label>
+							<input type="password" class="form-control" id="inputPassword4">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="inputAddress">Address</label>
+						<input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+					</div>
+					<div class="form-group">
+						<label for="inputAddress2">Address 2</label>
+						<input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label for="inputCity">City</label>
+							<input type="text" class="form-control" id="inputCity">
+						</div>
+						<div class="form-group col-md-4">
+							<label for="inputState">State</label>
+							<select id="inputState" class="form-control">
+								<option selected>Choose...</option>
+								<option>...</option>
+							</select>
+						</div>
+						<div class="form-group col-md-2">
+							<label for="inputZip">Zip</label>
+							<input type="text" class="form-control" id="inputZip">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="form-check">
+							<input class="form-check-input" type="checkbox" id="gridCheck">
+							<label class="form-check-label" for="gridCheck">
+								Check me out
+							</label>
+						</div>
+					</div>
+					<button type="submit" class="btn btn-primary">Sign in</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
+
+
+
+
+
+
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>ICRTBI2020 Registration</title>
+	<link rel="stylesheet" type="text/css" href="view.css" media="all">
+	<!-- <link href="payment.css" rel="stylesheet" type="text/css"></link> -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="view.js"></script>
+	<style type="text/css">
+		#li_1 {
+			list-style: none;
+		}
+
+		#li_2,
+		#li_3,
+		#li_4,
+		#li_5,
+		#li_6,
+		#li_7 {
+			list-style: none;
+		}
+	</style>
+</head>
+
+<body id="main_body">
 	<img id="top" src="top.png" alt="">
 	<div id="form_container">
-
-		<h1><a>5th International Conference on Recent Trends in Engineering and Technology</a></h1>
 		<form id="form_38599" class="appnitro" enctype="multipart/form-data" method="post" action="">
-					<div class="form_description">
-			<h2>5th International Conference on Recent Trends in Engineering and Technology</h2>
-			<p></p>
-		</div>
-			<ul>
+			<div class="form_description">
+				<p></p>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2"></div>
+					<div class="col-md-10">
+						<ul>
 
 
-<li id="li_1" >
-		<label class="description" for="element_1">Paper ID</label>
-		<div>
-			<input id="element_1" name="element_0" class="element text medium" type="text" maxlength="255" value=""/>
-		</div>
-		</li><br>
+							<li id="li_1">
+								<label class="description" for="element_1">Paper ID</label>
+								<div>
+									<input id="element_1" name="element_0" class="element text medium" type="text" maxlength="255" value="" />
+								</div>
+							</li><br>
 
-				<li id="li_1" >
-		<label class="description" for="element_1">Paper Title</label>
-		<div>
-			<input id="element_1" name="element_1" class="element text medium" type="text" maxlength="255" value=""/>
-		</div>
-		</li><br>		<li id="li_2" >
-		<label class="description" for="element_2">Corresponding Author Name</label>
-		<div>
-			<input id="element_2" name="element_2" class="element text medium" type="text" maxlength="255" value=""/>
-		</div>
-		</li><br>		<li id="li_3" >
-		<label class="description" for="element_3">Author Email </label>
-		<div>
-			<input id="element_3" name="element_3" class="element text medium" type="text" maxlength="255" value=""/>
-		</div>
-		</li><br>	
+							<li id="li_1">
+								<label class="description" for="element_1">Paper Title</label>
+								<div>
+									<input id="element_1" name="element_1" class="element text medium" type="text" maxlength="255" value="" />
+								</div>
+							</li><br>
+							<li id="li_2">
+								<label class="description" for="element_2">Corresponding Author Name</label>
+								<div>
+									<input id="element_2" name="element_2" class="element text medium" type="text" maxlength="255" value="" />
+								</div>
+							</li><br>
+							<li id="li_3">
+								<label class="description" for="element_3">Author Email </label>
+								<div>
+									<input id="element_3" name="element_3" class="element text medium" type="text" maxlength="255" value="" />
+								</div>
+							</li><br>
 
-		<li id="li_1" >
-		<label class="description" for="element_1">Affliation</label>
-		<div>
-			<input id="element_1" name="element_9" class="element text medium" type="text" maxlength="255" value=""/>
-		</div>
-		</li><br>
+							<li id="li_1">
+								<label class="description" for="element_1">Affliation</label>
+								<div>
+									<input id="element_1" name="element_9" class="element text medium" type="text" maxlength="255" value="" />
+								</div>
+							</li><br>
 
 
-			<li id="li_4" >
-		<label class="description" for="element_4">Name of Institution </label>
-		<div>
-			<input id="element_4" name="element_4" class="element text large" type="text" maxlength="255" value=""/>
-		</div>
-		</li><br>		<li id="li_5" >
-		<label class="description" for="element_5">Author Mobile </label>
-		<div>
-			<input id="element_5" name="element_5" class="element text medium" type="text" maxlength="255" value=""/>
-		</div>
-		</li><br>
-		<!-- <li id="li_5" >
+							<li id="li_4">
+								<label class="description" for="element_4">Name of Institution </label>
+								<div>
+									<input id="element_4" name="element_4" class="element text large" type="text" maxlength="255" value="" />
+								</div>
+							</li><br>
+							<li id="li_5">
+								<label class="description" for="element_5">Author Mobile </label>
+								<div>
+									<input id="element_5" name="element_5" class="element text medium" type="text" maxlength="255" value="" />
+								</div>
+							</li><br>
+							<!-- <li id="li_5" >
 		<label class="description" for="element_7">Category</label>
 		<div>
 			<select id="test" name="element_7" onchange="showDiv('hidden_div', this)">
@@ -115,27 +231,27 @@ if(isset($_POST['submit'])){
 
 
 
-		<li id="li_1" >
-		<label class="description" for="element_1">Category</label>
-		<div>
-			<select name="element_8" onchange="showDiv('hidden_div',this)">
-					<option value="1">STUDENT</option>
-					<option value="2">RESEARCH SCHOLAR</option>
-					<option value="3">ACADEMIA / R&D / INDUSTRY</option>
-					<option value="4">LISTENER</option>
-				</select>
-		</div>
-		</li>
-		<li id="li_1" >
-		<div>
-			<input type="checkbox" name="sem" value="1" onchange="check('hidden_div',this)">INCLUDE SEMINAR<br>
-		</div>
-		</li><br>
+							<li id="li_1">
+								<label class="description" for="element_1">Category</label>
+								<div>
+									<select name="element_8" onchange="showDiv('hidden_div',this)">
+										<option value="1">STUDENT</option>
+										<option value="2">RESEARCH SCHOLAR</option>
+										<option value="3">ACADEMIA / R&D / INDUSTRY</option>
+										<option value="4">LISTENER</option>
+									</select>
+								</div>
+							</li>
+							<li id="li_1">
+								<div>
+									<input type="checkbox" name="sem" value="1" onchange="check('hidden_div',this)">INCLUDE SEMINAR<br>
+								</div>
+							</li><br>
 
 
 
 
-		<!-- <li id="li_5">
+							<!-- <li id="li_5">
 			<div id="hidden_div">
 				<label>Amount</label><br>
 				<select name="element_8">
@@ -155,85 +271,86 @@ if(isset($_POST['submit'])){
 				</select>
 			</div>
 		</li><br> -->
-		<li id="li_5">
-			<label class="description" for="element_1">Discount Coupon</label>
-			<div>
-				<input type="text" name="element_9" placeholder="Enter Coupon Code">
+							<li id="li_5">
+								<label class="description" for="element_1">Discount Coupon</label>
+								<div>
+									<input type="text" name="element_9" placeholder="Enter Coupon Code">
+								</div>
+							</li><br>
+
+							<li id="li_5">
+								<label class="description" for="element_1">PRICE</label>
+								<div>
+									<input type="number" name="element_9" placeholder="Enter Coupon Code" id="hidden_div" value=6500 readonly>
+								</div>
+							</li><br>
+
+							<li class="buttons" id="li_5">
+								<input type="hidden" name="form_id" value="38599" />
+
+								<input id="saveForm" class="button_text" type="submit" name="submit" value="Make Payment" />
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+
+					</div>
+				</div>
 			</div>
-		</li><br>
 
-		<li id="li_5">
-			<label class="description" for="element_1">PRICE</label>
-			<div>
-				<input type="number" name="element_9" placeholder="Enter Coupon Code" id="hidden_div" value=6500 readonly>
-			</div>
-		</li><br>
-
-					<li class="buttons">
-			    <input type="hidden" name="form_id" value="38599" />
-
-				<input id="saveForm" class="button_text" type="submit" name="submit" value="Make Payment" />
-		</li>
-			</ul>
 		</form>
 
-<!-- 
+		<!-- 
 <div style="text-align: center;background-color: lightgreen;border: 2px solid green;border-radius: 50px;color: green;font-weight: bold;font-size: 20px;padding: 20px;margin: 10px">Transaction Sucess</div> -->
 
-<?php
+		<?php
 
 
 
-	
-if(isset($_GET['msg'])){
-    $id=$_SESSION['mtxnid'];
-    $message=$_GET['msg'];
-    $ref=$_GET['msg'];
-    $ref = json_decode($ref, true);
-	if(strcmp($message,'Transaction Failed')==0){
-		$query="UPDATE register set payment_status='failed' WHERE payment_id='$id'";
-		$query_run=mysqli_query($con,$query);
+
+		if (isset($_GET['msg'])) {
+			$id = $_SESSION['mtxnid'];
+			$message = $_GET['msg'];
+			$ref = $_GET['msg'];
+			$ref = json_decode($ref, true);
+			if (strcmp($message, 'Transaction Failed') == 0) {
+				$query = "UPDATE register set payment_status='failed' WHERE payment_id='$id'";
+				$query_run = mysqli_query($con, $query);
 		?>
-		<div style="text-align: center;background-color:#ff9999 ;border: 2px solid red;border-radius: 50px;color: red;font-weight: bold;font-size: 20px;padding: 20px;margin: 10px">Payment Failed</div>
-<?php
-}
-	else{
-		$query="UPDATE register set payment_status='success' WHERE payment_id='$id'";
-		$query_run=mysqli_query($con,$query);?>
-		<div style="text-align: center;background-color: lightgreen;border: 2px solid green;border-radius: 50px;color: green;font-weight: bold;font-size: 20px;padding: 20px;margin: 10px">Payment Sucess<br>
-			Your Ref Id : <?php echo $ref['refno']; ?><br>
-			Amount Paid : <?php echo $ref['amount']; ?>
-		</div>
+				<div style="text-align: center;background-color:#ff9999 ;border: 2px solid red;border-radius: 50px;color: red;font-weight: bold;font-size: 20px;padding: 20px;margin: 10px">Payment Failed</div>
+			<?php
+			} else {
+				$query = "UPDATE register set payment_status='success' WHERE payment_id='$id'";
+				$query_run = mysqli_query($con, $query); ?>
+				<div style="text-align: center;background-color: lightgreen;border: 2px solid green;border-radius: 50px;color: green;font-weight: bold;font-size: 20px;padding: 20px;margin: 10px">Payment Sucess<br>
+					Your Ref Id : <?php echo $ref['refno']; ?><br>
+					Amount Paid : <?php echo $ref['amount']; ?>
+				</div>
 
-	<?php }
-}
+		<?php }
+		}
 
-?>
+		?>
 	</div>
 	<script type="text/javascript">
-		function showDiv(divId, element)
-{
-	
-	var amount=[6500,7000,7500,1500];
-    document.getElementById(divId).value = element.value == 1 ? 0:amount[element.value-1] ;
-    document.getElementById(divId).value = element.value == 2 ? 0:amount[element.value-1] ;
-    document.getElementById(divId).value = element.value == 3 ? 0:amount[element.value-1] ;
-    document.getElementById(divId).value = element.value == 4 ? 0:amount[element.value-1] ;
-}
+		function showDiv(divId, element) {
 
-function check(divId, element){
-	var amt=parseInt(document.getElementById(divId).value);
-	if(element.checked){
-		document.getElementById(divId).value=amt+500;
-	}else{
-		document.getElementById(divId).value=amt-500;
-	}
-}
+			var amount = [6500, 7000, 7500, 1500];
+			document.getElementById(divId).value = element.value == 1 ? 0 : amount[element.value - 1];
+			document.getElementById(divId).value = element.value == 2 ? 0 : amount[element.value - 1];
+			document.getElementById(divId).value = element.value == 3 ? 0 : amount[element.value - 1];
+			document.getElementById(divId).value = element.value == 4 ? 0 : amount[element.value - 1];
+		}
 
-
-
-
-
+		function check(divId, element) {
+			var amt = parseInt(document.getElementById(divId).value);
+			if (element.checked) {
+				document.getElementById(divId).value = amt + 500;
+			} else {
+				document.getElementById(divId).value = amt - 500;
+			}
+		}
 	</script>
-	</body>
+</body>
+
 </html>
